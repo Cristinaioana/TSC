c9792ef28e51c367eb30bb9214ae56d1a7ddc982
CRISTINA-CALIN\Cristina <cristinaioanacalin@gmail.com>
2022-04-18 12:35:51 +0300

    lab8

---
+++ lab7 - regresie + coverage/tools/regression_test.bat
@@ -0,0 +1,17 @@
+::========================================================================================
+call clean.bat
+::========================================================================================
+call build.bat
+::========================================================================================
+cd ../sim
+
+call run_test.bat 123
+call run_test.bat 461
+call run_test.bat 524
+call run_test.bat 4381
+call run_test.bat 2134
+call run_test.bat 1523
+call run_test.bat 623
+call run_test.bat 5123
+call run_test.bat 8745
+call run_test.bat 2143
